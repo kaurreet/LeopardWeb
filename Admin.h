69d94267254ee58b9a55d4009dffcc37bfd435a5
Andy Le <161982740+Andy101Le@users.noreply.github.com>
2025-05-31 22:12:12 -0400

    Update Admin.h

--- Admin.h
+++ Admin.h
@@ -15,10 +15,10 @@ public:
 	void show_last_name();
 	void show_all();
 	void show_ID();
-	void add_course(string in_course);
-	void remove_course(string in_course);
-	void add_user(string in_user);
-	void remove_user(string in_user);
+	string add_course(string in_course_name, int in_CRN, string in_Department, string in_Instructor, string in_time, string in_Dofw, string in_Semester, int in_year, int in_credits);
+	string remove_course(string in_course_name, int in_CRN, string in_Department, string in_Instructor, string in_time, string in_Dofw, string in_Semester, int in_year, int in_credits);
+	string add_user(string in_user_add_f, string in_user_add_l, string in_user_add_ID, string in_user_add_Grad_year, string in_user_add_Major, string in_user_add_Email, int add_user_type, string user_add_Title, string user_add_YOH, string user_add_Department, string user_add_Office);
+	string remove_user(string in_user_removef, string in_user_removel, string in_user_remove_ID, string in_user_remove_Grad_year, string in_user_remove_Major, string in_user_remove_Email, int remove_user_type, string user_remove_Title, string user_remove_YOH, string user_remove_Department, string user_remove_Office);
 	void add_student_course(string in_student, string in_course);
 	void remove_student_course(string in_student, string in_course);
 	void search_roster(string in_course_search_roster);
