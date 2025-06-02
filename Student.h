ae079ca75b23920181f3ed5807842788f63d4b12
Andy Le <161982740+Andy101Le@users.noreply.github.com>
2025-06-01 22:15:23 -0400

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,11 +1,18 @@
 #pragma once
 #include "User.h"
+#include <set>
+extern "C" {
+#include "sqlite3.h"
+}
+
+using std::set;
 class Student :
 	public User
 {
 	string first_name;
 	string last_name;
 	string ID;
+	set<string> courses_enrolled;
 public:
 	//constructor
 	Student();
@@ -15,9 +22,10 @@ public:
 	void show_last_name();
 	void show_all();
 	void show_ID();
-	void search_course(string in_course);
-	void add_drop_course(string in_course, string in_add_drop);
-	void print_schedule();
+	void search_course(sqlite3* db, string course_add_drop, int Course_CRN, string department_course, string course_instructor, string course_start_time, string Meeting_times, string course_semester, int course_year, int course_credits);
+	string add_course(string course_add, int Course_CRN, string department_course, string course_instructor, string course_start_time, string Meeting_times, string course_semester, int course_year, int course_credits);
+	string remove_course(string course_remove, int Course_CRN, string department_course, string course_instructor, string course_start_time, string Meeting_times, string course_semester, int course_year, int course_credits);
+	string print_schedule();
 	//destructor
 	~Student();
 };
