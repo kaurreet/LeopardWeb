c934af782ecfebf0b7feeba362b34a2bb4ae19e5
Andy Le <161982740+Andy101Le@users.noreply.github.com>
2025-06-01 22:14:43 -0400

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -1,11 +1,17 @@
 #pragma once
 #include "Student.h"
+#include <set>
+extern "C" {
+#include "sqlite3.h"
+}
+using std::set;
 class Instructor :
     public User
 {
 	string first_name;
 	string last_name;
 	string ID;
+	set<string> courses_taught;
 public:
 	//constructor
 	Instructor();
@@ -15,9 +21,9 @@ public:
 	void show_last_name();
 	void show_all();
 	void show_ID();
-	void search_course(string in_course);
-	void print_class_list(string in_course);
-	void print_schedule();
+	void search_course(sqlite3* db, string course_add_drop, int Course_CRN, string department_course, string course_instructor, string course_start_time, string Meeting_times, string course_semester, int course_year, int course_credits);
+	void print_class_list(string &student_query);
+	string print_schedule();
 	//destructor
 	~Instructor();
 
