7fc9ca2f9efc1c2cab9624de6bf7af142d98a983
Andy Le <161982740+Andy101Le@users.noreply.github.com>
2025-06-16 14:50:05 -0400

    Update Student.h

--- Student.h
+++ Student.h
@@ -9,22 +9,18 @@ using std::set;
 class Student :
 	public User
 {
+public:
 	string first_name;
 	string last_name;
-	string ID;
+	int ID;
 	set<string> courses_enrolled;
-public:
+
 	//constructor
-	Student();
-	Student(string in_fname, string in_lname, string in_ID);
+	Student(string in_fname, string in_lname, int in_ID);
 	//methods
-	void show_first_name();
-	void show_last_name();
-	void show_all();
-	void show_ID();
 	void search_course(sqlite3* db, string course_add_drop, int Course_CRN, string department_course, string course_instructor, string course_start_time, string Meeting_times, string course_semester, int course_year, int course_credits);
-	string add_course(string course_add, int Course_CRN, string department_course, string course_instructor, string course_start_time, string Meeting_times, string course_semester, int course_year, int course_credits);
-	string remove_course(string course_remove, int Course_CRN, string department_course, string course_instructor, string course_start_time, string Meeting_times, string course_semester, int course_year, int course_credits);
+	string add_course(sqlite3* DB, string in_course_name, int ID);
+	string remove_course(sqlite3* DB, string in_course_name, int ID);
 	string print_schedule();
 	//destructor
 	~Student();
