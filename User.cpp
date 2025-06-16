7eff54102f46482fd1dd4acc31748feccead3cc2
Andy Le <161982740+Andy101Le@users.noreply.github.com>
2025-06-16 14:50:28 -0400

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -8,28 +8,28 @@ User::User() {
 	first_name = "Andy";
 	last_name = "Le";
 }
-User::User(string in_fname, string in_lname, string in_ID) {
+User::User(string in_fname, string in_lname, int in_ID) {
 	first_name = in_fname;
 	last_name = in_lname;
 	ID = in_ID;
 }
 //methods
-void User::show_first_name() {
-	cout << first_name;
-	//return first_name;
+string User::get_first_name(string in_first_name) {
+	first_name = in_first_name;
+	return first_name;
 	
 }
-void User::show_last_name() {
-	cout << last_name;
-	//return last_name;
+string User::get_last_name(string in_last_name) {
+	last_name = in_last_name;
+	return last_name;
 	
 }
-void User::show_ID() {
-	cout << ID;
-	//return ID;
+int User::get_ID(int in_ID) {
+	ID = in_ID;
+	return ID;
 	
 }
-void User::show_all() {
+void User::print_all() {
 	cout << first_name << endl;
 	cout << last_name << endl;
 	cout << ID << endl;
