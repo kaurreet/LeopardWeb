57c2f0029cb8349aa40dd3816c9f0ed448b18119
Andy Le <161982740+Andy101Le@users.noreply.github.com>
2025-06-16 14:49:29 -0400

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -8,22 +8,22 @@ using std::set;
 class Instructor :
     public User
 {
+public:
 	string first_name;
 	string last_name;
-	string ID;
+	int ID;
 	set<string> courses_taught;
-public:
+
 	//constructor
-	Instructor();
-	Instructor(string in_fname, string in_lname, string in_ID);
+	Instructor(string in_fname, string in_lname, int in_ID);
 	//methods
 	void show_first_name();
 	void show_last_name();
 	void show_all();
 	void show_ID();
-	void search_course(sqlite3* db, string course_add_drop, int Course_CRN, string department_course, string course_instructor, string course_start_time, string Meeting_times, string course_semester, int course_year, int course_credits);
+	string search_course(sqlite3* db, string course_add_drop, string in_fname, string in_lname);
 	void print_class_list(string &student_query);
-	string print_schedule();
+	string print_schedule(string in_fname, string in_lname);
 	//destructor
 	~Instructor();
 
