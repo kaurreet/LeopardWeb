18018316a423c74d0ba900f2d8b8980ca273398a
Andy Le <161982740+Andy101Le@users.noreply.github.com>
2025-06-16 14:50:40 -0400

    Update User.h

--- User.h
+++ User.h
@@ -1,23 +1,24 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <set>
 using std::string;
 
 class User
 {
+public:
 	string first_name;
 	string last_name;
-	string ID;
+	int ID;
 
-public:
 	//constructor
 	User();
-	User(string in_fname, string in_lname, string in_ID);
+	User(string in_fname, string in_lname, int in_ID);
 	//methods
-	virtual void show_first_name();
-	virtual void show_last_name();
-	virtual void show_ID();
-	virtual void show_all();
+	virtual string get_first_name(string in_first_name);
+	virtual string get_last_name(string in_last_name);
+	virtual int get_ID(int in_ID);
+	virtual void print_all();
 	//destructor
 	~User();
 };
