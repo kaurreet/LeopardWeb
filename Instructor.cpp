15746629cb2d82aabcd0e5dec0e1b4ed3fffc372
Andy Le <161982740+Andy101Le@users.noreply.github.com>
2025-06-24 15:33:02 -0400

    Update Instructor.cpp

--- Instructor.cpp
+++ Instructor.cpp
@@ -5,11 +5,12 @@ using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
-extern "C" {
-#include "sqlite3.h"
-}
 //constructor
-Instructor::Instructor(string in_fname, string in_lname, int in_ID) {
+Instructor::Instructor() {
+	first_name = "Andy";
+	last_name = "Le";
+}
+Instructor::Instructor(string in_fname, string in_lname, string in_ID) {
 	first_name = in_fname;
 	last_name = in_lname;
 	ID = in_ID;
@@ -37,55 +38,23 @@ void Instructor::show_all() {
 	//return first_name, last_name, ID;
 
 }
-//void Instructor::search_course(sqlite3* db, string course_add_drop, int Course_CRN, string department_course, string course_instructor, string course_start_time, string Meeting_times, string course_semester, int course_year, int course_credits) {
-//	sqlite3_stmt* stmt;
-//	//int rc = sqlite3_open("assignment3.db", &db);
-//	const char* sql = R"(
-//        SELECT * FROM COURSE
-//        WHERE CRN = ? AND TITLE = ? AND DEPARTMENT = ? AND INSTRUCTOR = ? AND TIME = ? AND
-//              DoftW = ? AND SEMESTER = ? AND YEAR = ? AND CREDITS = ?)";
-//
-//	int rc = sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr);
-//	if (rc != SQLITE_OK) {
-//		std::cerr << "Failed to prepare statement. Error: " << sqlite3_errmsg(db) << std::endl;
-//		return;
-//	}
-//
-//	sqlite3_bind_int(stmt, 1, Course_CRN);
-//	sqlite3_bind_text(stmt, 2, course_add_drop.c_str(), -1, SQLITE_TRANSIENT);
-//	sqlite3_bind_text(stmt, 3, department_course.c_str(), -1, SQLITE_TRANSIENT);
-//	sqlite3_bind_text(stmt, 4, course_instructor.c_str(), -1, SQLITE_TRANSIENT);
-//	sqlite3_bind_text(stmt, 5, course_start_time.c_str(), -1, SQLITE_TRANSIENT);
-//	sqlite3_bind_text(stmt, 6, Meeting_times.c_str(), -1, SQLITE_TRANSIENT);
-//	sqlite3_bind_text(stmt, 7, course_semester.c_str(), -1, SQLITE_TRANSIENT);
-//	sqlite3_bind_int(stmt, 8, course_year);
-//	sqlite3_bind_int(stmt, 9, course_credits);
-//
-//	rc = sqlite3_step(stmt);
-//	if (rc == SQLITE_ROW) {
-//		cout << "Course found in the database!" << endl;
-//	}
-//	else {
-//		cout << "Course NOT found in the database!" << endl;
-//	}
-//	sqlite3_finalize(stmt);
-//}
-string Instructor::search_course(sqlite3* db, string course_add_drop, string in_fname, string in_lname) {
-	//string search_print_roster;
-	//search_print_roster = "SELECT * FROM ";
-	string sql = "SELECT STUDENT.NAME, STUDENT.SURNAME, STUDENT.ID "
-				 "FROM STUDENT "
-				 "JOIN REGISTERED ON STUDENT.ID = REGISTERED.StudentID "
-				 "JOIN COURSE ON REGISTERED.CRN = COURSE.CRN "
-				 "WHERE COURSE.TITLE = '" + course_add_drop + "' "
-				 "AND COURSE.INSTRUCTOR = '" + in_fname + " " + in_lname + "';";
-	return sql;
+void Instructor::search_course(string in_course) {
+	if (courses_taught.find(in_course) != courses_taught.end())
+		cout << in_course << " is on this instructor's schedule." << endl;
+	else
+		cout << in_course << " is not in the instructor's course list." << endl;
 }
-void Instructor::print_class_list(string &student_query) {
-	student_query = "SELECT * FROM STUDENT;";
+void Instructor::print_class_list(string in_course) {
+	cout << "Class List for " << in_course << "has been Printed!" << endl;
+
 }
-string Instructor::print_schedule(string in_fname, string in_lname) {
-	return "SELECT * FROM COURSE WHERE INSTRUCTOR = '" + in_fname + " " + in_lname + "';";
+void Instructor::print_schedule() {
+	if (courses_taught.empty())
+		cout << "Schedule Printed!" << endl;
+	else
+		cout << "Instructor's Schedule:" << endl;
+		
+
 }
 //destructor
 Instructor::~Instructor() {
