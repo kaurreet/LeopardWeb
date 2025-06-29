2282710c8e9a5b6006bccfdf9e71a7b4ee236775
Andy Le <161982740+Andy101Le@users.noreply.github.com>
2025-06-29 17:55:12 -0400

    Update Student.h

--- Student.h
+++ Student.h
@@ -17,7 +17,7 @@ public:
 	//constructor
 	Student(string in_fname, string in_lname, int in_ID);
 	//methods
-	void search_course(sqlite3* db, string course_add_drop, int Course_CRN, string department_course, string course_instructor, string course_start_time, string Meeting_times, string course_semester, int course_year, int course_credits);
+	string search_course(sqlite3* db, string course_add_drop, int in_CRN, string in_dep, string in_instruct, string in_parameter);
 	string add_course(sqlite3* DB, string in_course_name, int ID);
 	string remove_course(sqlite3* DB, string in_course_name, int ID);
 	string print_schedule();
