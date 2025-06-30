984bc4db3d0c68828fef50381eb740d27f4149a8
Andy Le <161982740+Andy101Le@users.noreply.github.com>
2025-06-29 23:26:15 -0400

    Update Course.cpp

--- Course.cpp
+++ Course.cpp
@@ -6,6 +6,8 @@
 #include <stdio.h>
 #include <string>
 #include <set>
+
+Course::Course() {};
 Course::Course(int in_crn, string in_title, string in_department, string in_instructor, string in_time, string in_days, string in_semester, int in_year, int in_credits) {
      crn = in_crn;
      title = in_title;
@@ -17,6 +19,42 @@ Course::Course(int in_crn, string in_title, string in_department, string in_inst
      year = in_year;
      credits = in_credits;
 }
+Course::Course(string in_title) {
+    title = in_title;
+}
+
+void Course::set_CRN(int in_CRN) {
+    crn = in_CRN;
+}
+
+void	Course::set_title(string in_title){
+    title = in_title;
+}
+void	Course::set_department(string in_department){
+    department = in_department;
+}
+void	Course::set_instructor(string in_instructor){
+    instructor = in_instructor;
+}
+void	Course::set_starttime(string in_starttime){
+    time = in_starttime;
+}
+void	Course::set_meetingtime(string in_meetingtime){
+    days = in_meetingtime;
+}
+void	Course::set_semester(string in_semester){
+    semester = in_semester;
+}
+void	Course::set_year(int in_year){
+    year = in_year;
+}
+void	Course::set_credits(int in_credits){
+    credits = in_credits;
+}
+void	Course::set_studentID(int in_ID){
+    studentID = in_ID;
+}
+
 int Course::get_CRN(){
     return crn;
 }
@@ -43,4 +81,8 @@ int Course::get_year(){
 }
 int Course::get_credits(){
     return credits;
+}
+
+int Course::get_studentID() {
+    return studentID;
 }
