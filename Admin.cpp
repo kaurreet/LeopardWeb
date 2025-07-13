54e799d1ab05803781e5bdc28fdfe8d2d6ae202e
Andy Le <161982740+Andy101Le@users.noreply.github.com>
2025-07-13 17:45:43 -0400

    Update Admin.cpp

--- Admin.cpp
+++ Admin.cpp
@@ -10,6 +10,10 @@ using std::cout;
 using std::endl;
 using std::string;
 
+Admin::Admin() {
+
+}
+
 Admin::Admin(string in_fname, string in_lname, int in_ID) {
 	first_name = in_fname;
 	last_name = in_lname;
